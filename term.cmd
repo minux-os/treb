81b966f6b7abe2c8166fa2ef5d16df05fe26343e
minux-os <selfospowerpoint@vk.com>
2026-04-14 11:15:29 +0500

    Update term.cmd

--- term.cmd
+++ term.cmd
@@ -1,14 +1,20 @@
 @echo off
 chcp 65001 >nul
-title Treb Lock Manager
-echo Welcome to Treb!
+echo WorldBIOS
+timeout /t 3 >nul
+dir
+echo Found! folder: minux
+timeout /t 3 >nul
+cls
+title Minux Lock Manager
+echo Welcome to Minux NEXT!
 echo(
 goto login
 :login
 echo Kostya Aidin (c) 2026 All rights reserved > session.log
 echo [ %date% %time% ] Login start >> session.log
 set /p command="%COMPUTERNAME% login: "
-if "%command%"=="treb" goto usrpass
+if "%command%"=="user" goto usrpass
 )   else   (
     echo User not found
     echo [ %date% %time% ] Login failed >> session.log
@@ -18,8 +24,8 @@ if "%command%"=="treb" goto usrpass
 
 :usrpass
 echo [ %date% %time% ] password verification >> session.log
-set /p command="user login: "
-if "%command%"=="minux" goto minux
+set /p pass="user login: "
+if "%pass%"=="1234" goto minux
 )   else   (
     echo [ %date% %time% ] password incorrect >> session.log
     echo Password incorrect!
@@ -27,6 +33,7 @@ if "%command%"=="minux" goto minux
 )
 
 :minux
+title Minux
 echo [ %date% %time% ] Login successful >> session.log
 echo Welcome To...
 
@@ -41,24 +48,27 @@ echo ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝ ╚════�
 echo(
 
 echo Kostya Aidin (c) 2026 All rights reserved
-set /p command="user@treb:~$ "
+set /p command="user@minux:~$ "
 
 if "%command%"=="vget" goto vget
-if "%command%"=="vget --dns" goto vgetdns
+if "%command%"=="vget SendWebRequest " goto vgetwr
 if "%command%"=="su nam update" goto updte
 if "%command%"=="su nam -u" goto updte
 if "%command%"=="date" goto date_cmd
 if "%command%"=="minux" call boot.cmd
 if "%command%"=="sysfetch" goto sysftch
 if "%command%"=="su restart --force" call minux/restart.cmd
-if "%command%"=="restart" echo Restarting... && timeout /t 3 && cls && goto minux
+if "%command%"=="restart" echo Restarting... && timeout /t 3 && cls && call minux/restart.cmd
 if "%command%"=="clear" cls
 if "%command%"=="vook cmd" cmd
 if "%command%"=="vook pwsh" powershell
-if "%command%"=="vook restart" call minux/restart.cmd.
+if "%command%"=="vook restart" call minux/restart.cmd
 if "%command%"=="vook run" call minux/vook/run.cmd
 if "%command%"=="kill term.cmd" exit
+if "%command%"=="mnxset sys=NX" goto mnxsetnxs
 if "%command%"=="su mnxset sys=NX" goto mnxsetnx
+if "%command%"=="mnxset sys=null" goto mnxsetnxs
+if "%command%"=="su mnxset sys=null" goto mnxsetnll
 if "%command%"=="logout" cls & goto login
 if "%command%"=="tit" goto tit
 if "%command%"=="su adduser" goto addusersu
@@ -66,7 +76,13 @@ if "%command%"=="users" goto users
 if "%command%"=="gamehub" goto gamehub
 if "%command%"=="exit" goto exit
 if "%command%"=="license" goto lic
-if "%command%"=="treb -c" goto create
+if "%command%"=="calc" goto calc
+if "%command%"=="yt" goto yt
+if "%command%"=="loop" goto loop
+if "%command%"=="loop -s" goto loops
+if "%command%"=="clock" goto clock
+if "%command%"=="ee 1" goto sysee1
+if "%command%"=="treb -c" goto sysee1
 :date
 echo date %date%
 goto minux
@@ -85,8 +101,7 @@ cls
 echo URL: %url%
 echo Packages: %p%
 ping %url% -n %p%
-cls
-echo Send packages: %p%
+echo Sent packages: %p%
 pause
 cls
 echo Site: %url% > vget.log
@@ -146,11 +161,39 @@ pause
 goto minux
 
 
+:mnxsetnxs
+echo Access denied! Are you root?
+goto minux
+
+
 :mnxsetnx
-echo mnxset: Accept to 10 s
-timeout /t 3
-del term.cmd
-exit
+echo root command:
+goto mnxsetnxrootpass
+
+
+:mnxsetnxrootpass
+echo [ %date% %time% ] >> session.log
+set /p pass="root login: "
+if "%pass%"=="root" goto sysnx
+)   else   (
+    echo [ %date% %time% ] root: password incorrect >> session.log
+    echo Password incorrect!
+    goto mnxsetnxrootpass
+)
+
+:sysnx
+echo accept to 3s
+timeout /t 3 >nul
+echo The value of the "sys" parameter has been changed to "NX"
+timeout /t 3 >nul
+cls
+goto x
+
+:x
+timeout /t 1 >nul
+goto x
+
+
 
 :tit
 set /p tit=%COMPUTERNAME%@tit:~$
@@ -171,26 +214,7 @@ mkdir Documents
 mkdir Music
 mkdir Downloads
 echo User %user% created!
-echo Information correct?
-echo User: %user%
-echo (y/n)
-set /p ans="y/n: "
-if "%command%"=="y" goto usery
-if "%command%"=="n" goto usern
-
-
-:usery
-echo Thank you!
-pause
-cls
 goto minux
-
-
-:usern
-del /%user%
-echo %user% deleted!
-goto minux
-
 
 :users
 cd users
@@ -233,12 +257,130 @@ pause
 exit
 
 
+:calc
+set /p num1="Enter 1 number: "
+set /p num2="Enter 2 number: "
+set /a result=%num1%+%num2%
+echo %num1% + %num2% = %result%
+goto minux
+
 :lic
 echo 1. DO NOT EDIT THIS OS [/\] 
 REM Защита
 echo 2. EDIT YOUR DISTRO TO BASE MINUX [\/]
 echo /\ - no
 echo \/ - yes
+goto minux
+
+
+
+
+
+
+
+:yt
+cls
+set /p ytid="Enter ID: "
+start https://www.youtube.com/watch?v=%ytid%
+set ytid=
+goto minux
+
+
+
+:vgetwr
+cls
+set /p wru="Enter URL for method: WRU: "
+powershell Invoke
+
+
+:loop
+echo /sys
+echo /init
+echo /system
+set /p loop="Enter host:"
+mkdir loop
+cd loop
+mkdir %loop%
+echo testing...
+ping %loop%
+echo Hacking...
+echo site: %loop% >> hack.log
+echo Log created by loop on %date% %time% >> hack.log
+set loophack=hack.log
+ping %loop% >> %loophack%
+goto minux
+
+
+
+
+
+:loops
+echo /sys
+echo /init
+echo /system
+set /p loop="Enter host:"
+mkdir loop
+cd loop
+mkdir %loop%
+echo testing...
+ping %loop%
+echo Hacking...
+echo site: %loop% >> hack.log
+echo Log created by loop on %date% %time% >> hack.log
+set loophack=hack.log
+ping %loop% >> %loophack%
+cd ..
+copy term.cmd loop\term.cmd
+goto minux
+
+
+:clock
+set serv=time.google.com
+echo Reco:
+echo set serv=time.google.com
+echo set serv=time.android.com
+echo set serv=time.google.com
+echo set serv=time.apple.com
+w32tm /stripchart /computer:%serv% /dataonly /samples:1
+timeout /t 1 >nul
+cls
+goto clock
+
+
+
+:mnxsetnll
+echo root command:
+goto mnxsetnllrootpass
+
+
+:mnxsetnllrootpass
+echo [ %date% %time% ] >> session.log
+set /p pass="root login: "
+if "%pass%"=="root" goto sysnll
+)   else   (
+    echo [ %date% %time% ] root: password incorrect >> session.log
+    echo Password incorrect!
+    goto mnxsetnllrootpass
+)
+
+
+:sysnll
+echo accept to 3s
+timeout /t 3 >nul
+echo The value of the "sys" parameter has been changed to "null"
+timeout /t 3 >nul
+cls
+goto x
+
+:sysee1
+echo accept to 3s
+timeout /t 3 >nul
+echo The value of the "ee" parameter has been changed to "1"
+timeout /t 3 >nul
+cls
+echo Easter Egg 1 Unlocked!
+pause
+echo The value of the "ee" parameter has been changed to "0"
 goto minux
 
 
@@ -260,11 +402,6 @@ echo Name: %cr%
 echo Status: builded!
 echo Made with treb
 goto minux
-
-
-
-
-
 )   else   (
     echo This is not command!
     goto minux
